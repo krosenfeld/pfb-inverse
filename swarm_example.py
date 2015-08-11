70ffcbfb31ff9bb292a28c2fb2e705d3a569abd8
Katherine Rosenfeld <krosenf@gmail.com>
2015-08-11 03:42:52 -0700

    cuda pfb inverse

--- swarm_example.py
+++ swarm_example.py
@@ -5,21 +5,10 @@ from time import time
 import resource
 from datetime import datetime
 import cross_corr
-
-#from cvxopt import matrix
-#import pfb
-#from cvxopt.lapack import pbsv
+from numpy.fft import irfft
 
 # Script based off of 
 # http://nbviewer.ipython.org/github/jrs65/pfb-inverse/blob/master/notes.ipynb
-
-# Changes:
-#   in inervse_pfb:
-#     la.solveh_banded produced an error:
-#     ** On entry to DPBSV parameter number  8 had an illegal value
-#     This is a problem for the scipy 0.7.2, but should be fixed for later
-#     versions.  I have opted to use pbsv from cvxopt instead.
-#
 
 def sinc_window(ntap, lblock):
     """Sinc window function.
@@ -80,7 +69,24 @@ def hamming(ntap, lblock):
     
     return np.hamming(ntap * lblock)
 
-def pfb(timestream, nfreq, ntap=4, window=hamming):
+def boxcar(ntap, lblock):
+    """boxcar window function.
+    
+    Parameters
+    ----------
+    ntaps : integer
+        Number of taps.
+    lblock: integer
+        Length of block.
+        
+    Returns
+    -------
+    window : np.ndarray[ntaps * lblock]
+    """
+    
+    return np.ones(ntap * lblock)
+
+def pfb(timestream, nfreq, ntap=4, window=hamming, drop_nyquist = True):
     """Perform the SWARM PFB on a timestream.
     
     Parameters
@@ -88,10 +94,11 @@ def pfb(timestream, nfreq, ntap=4, window=hamming):
     timestream : np.ndarray
         Timestream to process
     nfreq : int
-        Number of frequencies we want out (probably should be odd
-        number because of Nyquist)
+        Number of frequencies we want out
     ntaps : int
         Number of taps.
+    drop_nyquist : True
+	Set Nyquist to zero like SWARM
 
     Returns
     -------
@@ -110,7 +117,7 @@ def pfb(timestream, nfreq, ntap=4, window=hamming):
     
     # Window function
     w = window(ntap, lblock)
-    
+
     # Iterate over blocks and perform the PFB
     for bi in range(nblock):
         # Cut out the correct timestream section
@@ -120,7 +127,10 @@ def pfb(timestream, nfreq, ntap=4, window=hamming):
         ft = np.fft.rfft(ts_sec * w)
         
         # Choose every n-th frequency
-        spec[bi] = ft[::ntap]
+	if (drop_nyquist):
+        	spec[bi,:-1] = ft[:-ntap:ntap]
+	else:
+        	spec[bi] = ft[::ntap]
         
     return spec
 
@@ -164,8 +174,7 @@ def band_mv(A, kl, ku, n, m, x, trans=False):
     y = np.zeros(n if trans else m, dtype=np.float64)
    
     lda = kl + ku + 1
-    
-    #dgbmv.dgbmv('T' if trans else 'N', m, kl, ku, 1.0, A, x, 1, 0.0, y, 1)
+
     dgbmv.dgbmv('T' if trans else 'N', m, n, kl, ku, 1.0, A, x, 1, 0.0, y, 1)
     
     return y
@@ -217,21 +226,20 @@ def inverse_pfb(ts_pfb, ntap, window=hamming):
         yh = la.solveh_banded(band_PPT, pseudo_ts[i_off])
 
         # Project into timestream estimate
-        rec_ts[i_off] = band_mv(band_P, 0, 3, ntsblock, nblock, yh, trans=True)
+        rec_ts[i_off] = band_mv(band_P, 0, ntap-1, ntsblock, nblock, yh, trans=True)
 
-	# using cvxopt
-        # Solve for intermediate vector
-        #A = matrix(band_PPT)
-        #B = matrix(pseudo_ts[i_off])
-        #pbsv(A,B,uplo='U')
-
-        # Project into timestream estimate
-        #rec_ts[i_off] = band_mv(band_P, 0, 3, ntsblock, nblock, B, trans=True)
-        
     # Transpose timestream back
     rec_ts = rec_ts.T.copy()
     
     return rec_ts
+
+def full_banded(diags):
+    (u,M) = diags.shape
+    ans = np.zeros((M,M))
+    for k in range(u-1):
+        ans += np.diag((diags[k])[u-k-1:],k=u-k-1) + np.diag((diags[k])[u-k-1:],k=-u+k+1).conj()
+    ans += np.diag((diags[-1]),k=0) 
+    return ans
 
 ############################################################################
 ############################################################################
@@ -256,14 +264,18 @@ if __name__ == "__main__":
   plt.close('all')
 
   # setup 
+  show_figs = False
+  window = boxcar
   #window = hamming
-  window = sinc_hanning
-  num_taps = 4
-  num_bframes = 1
-  num_samples = SWARM_SAMPLES_PER_WINDOW * (SWARM_TRANSPOSE_SIZE*num_bframes + num_taps - 1)
-  #num_snapshots = 1
-  #num_samples = SWARM_SAMPLES_PER_WINDOW * (num_snapshots + num_taps - 1)
+  #window = sinc_hanning
+  #num_taps = 2	# = M
+  num_taps = 5	# = M
+  #num_taps = 1
+  num_bframes = 2
+  num_samples = SWARM_SAMPLES_PER_WINDOW*(SWARM_TRANSPOSE_SIZE*num_bframes + num_taps - 1)
   num_freq = SWARM_SAMPLES_PER_WINDOW/2 + 1 # include nyquist for now
+
+  delay = SWARM_SAMPLES_PER_WINDOW
 
   # Generate a white noise timestream
   tic = datetime.now()
@@ -275,45 +287,90 @@ if __name__ == "__main__":
   spec_pfb = pfb(ts,num_freq,ntap=num_taps,window=window)
   print 'performing PFB takes:', (datetime.now()-tic).total_seconds()
 
-
-  # Perform the inverse
+  # Perform the inverse (2.3 seconds versus 0.130 s for FFT, x18 slower)
   tic = datetime.now()
-  rts = inverse_pfb(spec_pfb, 4, window=window)
+  rts = inverse_pfb(spec_pfb, num_taps, window=window)
   print 'inverse pfb takes:', (datetime.now()-tic).total_seconds()
-
-  # show a small section of the PFB (real part)
-  plt.figure()
-  plt.imshow(spec_pfb[:10].real, interpolation='nearest', aspect='auto', cmap='RdBu')
-  plt.colorbar()  
-
-  # show time series residuals
-  plt.figure()
-  plt.plot((ts - rts.ravel())[:np.min([ 5000000,ts.size ])])
-  plt.ylabel('residual')
-
-  # plot standard deviation of residuals for blocks of data (128)
-  block_res = ts.reshape(-1, rts.shape[-1]) - rts
-  plt.figure()
-  plt.title("Standard deviation of residuals")
-  plt.semilogy(block_res.std(axis=1))
-  plt.xlabel("Snapshots")
 
   # cross correlate 
   s_0x1,S_0x1,s_peaks = cross_corr.corr_FXt(ts,rts.ravel())
-  plt.figure()
-  plt.stem(s_0x1[:20])
-  plt.ylabel('Corr coeff')
-  plt.xlabel("lag")
-  plt.xlim([-0.4,20])
+
+  # print stats:
+  print '\n\nPFB:\nMemory usage: %d bytes (%8.6f GB).' % (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss*1024,resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/2.0**20)
+  print 'mean squared error: {0:g}'.format(np.mean((ts - rts.ravel())**2))
+  print 'using {0:g} ms = {1:g} GB'.format(num_samples / SWARM_RATE * 1e3, ts.nbytes / 1e9)
+  print 'corr coef: {0:g}'.format(s_0x1[0])
+
+  if show_figs:
+    # show a small section of the PFB (real part)
+    plt.figure()
+    plt.imshow(spec_pfb[:10].real, interpolation='nearest', aspect='auto', cmap='RdBu')
+    plt.colorbar()  
+  
+    # show time series residuals
+    plt.figure()
+    plt.plot((ts - rts.ravel())[:np.min([ 5000000,ts.size ])])
+    plt.ylabel('IPFB residual')
+
+    # plot standard deviation of residuals for blocks of data (128)
+    block_res = ts.reshape(-1, rts.shape[-1]) - rts
+    plt.figure()
+    plt.title("Standard deviation of residuals (IPFB)")
+    plt.semilogy(block_res.std(axis=1))
+    plt.xlabel("Snapshots")
+
+    plt.figure()
+    plt.stem(s_0x1[:20])
+    plt.ylabel('Corr coeff (IPFB)')
+    plt.xlabel("lag")
+    plt.xlim([-0.4,20])
+
+
+  ###############
+  # iFFT
+  # Perform the inverse FFT
+  tic = datetime.now()
+  prts = irfft(spec_pfb,axis=-1)
+  print 'irfft takes:', (datetime.now()-tic).total_seconds()
+
+  #s_0x1,S_0x1,s_peaks = cross_corr.corr_FXt(ts[delay:delay+prts.size],prts.ravel(),fft_window_size=32768)
+  s_0x1,S_0x1,s_peaks = cross_corr.corr_FXt(ts[delay:delay+prts.size],prts.ravel(),fft_window_size=32768*64)
+
+  # print stats:
+  print '\n\niFFT:\nMemory usage: %d bytes (%8.6f GB).' % (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss*1024,resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/2.0**20)
+  print 'mean squared error: {0:g}'.format(np.mean((ts - rts.ravel())**2))
+  print 'using {0:g} ms = {1:g} GB'.format(num_samples / SWARM_RATE * 1e3, ts.nbytes / 1e9)
+  print 'corr coef: {0:g}'.format(s_0x1[0])
+  print 'corr coef: {0:g}'.format(np.max(s_0x1))
+
+  if show_figs:
+
+    # show time series residuals
+    plt.figure()
+    #plt.plot((ts[(num_taps-1)*32768:] - prts.ravel())[:np.min([ 5000000,prts.size ])])
+    #plt.plot((ts[(num_taps-1)*32768:] - prts.ravel())[:np.min([ 5000,prts.size ])])
+    plt.plot((ts[delay:delay+prts.size] - prts.ravel())[:np.min([ 5000,prts.size ])])
+    plt.ylabel('IFFT residual')
+  
+    # plot standard deviation of residuals for blocks of data (128)
+    #block_res = ts.reshape(-1, prts.shape[-1])[num_taps-1,:] - prts
+    block_res = ts.reshape(-1, prts.shape[-1])[1+prts.shape[0],:] - prts
+    plt.figure()
+    plt.title("Standard deviation of residuals (IFFT)")
+    plt.semilogy(block_res.std(axis=1))
+    plt.xlabel("Snapshots")
+  
+    # cross correlate 
+    #s_0x1,S_0x1,s_peaks = cross_corr.corr_FXt(ts,prts.ravel(),fft_window_size=32768*32)
+    plt.figure()
+    plt.stem(s_0x1[:20])
+    plt.ylabel('Corr coeff (IFFT)')
+    plt.xlabel("lag")
+    plt.xlim([-0.4,20])
+
 
   # show figures
   plt.ion()
   plt.show()
-
-  # print stats:
-  print 'Memory usage: %d bytes (%8.6f GB).' % (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss*1024,resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/2.0**20)
-  print 'mean squared error: {0:g}'.format(np.mean((ts - rts.ravel())**2))
-  print 'using {0:g} ms = {1:g} GB'.format(num_samples / SWARM_RATE * 1e3, ts.nbytes / 1e9)
-  print 'corr coef: {0:g}'.format(s_0x1[0])
 
 
